50fd98ae6c3c2eb7cfc8c9843f566eae30e22f2b
kimpearl3599 <kimpearl3599@gmail.com>
2026-02-19 10:01:23 +0900

    chore: ROS2 패키지 구조 설정
    - package.xml: rclpy, dsr_msgs2 등 의존성 정의
    - setup.py, setup.cfg: Python 패키지 빌드 설정
    - .gitignore: build, install, log 디렉토리 제외

---
+++ src/my_ros2_assignment/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+import os
+from glob import glob
+
+package_name = 'my_ros2_assignment'
+
+setup(
+    name=package_name,
+    version='0.1.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+        (os.path.join('share', package_name, 'scripts'), glob('scripts/*.sh')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='user',
+    maintainer_email='user@todo.todo',
+    description='ROS2 두산 E0509 로봇암 시뮬레이션 제어 GUI',
+    license='Apache-2.0',
+    entry_points={
+        'console_scripts': [
+            'my_node = my_ros2_assignment.main_node:main',
+        ],
+    },
+)
